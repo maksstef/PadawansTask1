745e3b8ab6dee8565d1329e058303f2944c84ed4
mxmstfnk <boss.canta@mail.ru>
2019-02-09 16:36:11 +0300

    1 attempt

---
+++ Population.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PadawansTask1
+{
+    public class Population
+    {
+        public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
+        {
+            int year = 1;
+            while (initialPopulation <= currentPopulation)
+            {
+                initialPopulation = (int)((visitors + initialPopulation) * (1+percent/100));
+                year++;
+            }
+            return year;
+        }
+    }
+}
